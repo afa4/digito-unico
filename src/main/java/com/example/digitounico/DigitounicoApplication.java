9ee8164e7cbfb50473a60b8f5c601c612da2b883
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-09 09:01:05 -0300

    initial commit

---
+++ src/main/java/com/example/digitounico/DigitounicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitounico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitounicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitounicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/digitounico/DigitounicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digitounico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitounicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
