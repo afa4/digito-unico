14bc4c550e1d21c3acddeb68e0306084acc7d623
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-10 16:27:04 -0300

    testing when getSingleDigit repeat parameter is <= 0

--- src/main/java/com/example/digitounico/service/SingleDigitService.java
+++ src/main/java/com/example/digitounico/service/SingleDigitService.java
@@ -10,23 +10,21 @@ public class SingleDigitService {
 
     private Map<String, Integer> lastTenCalculations = new HashMap<>();
 
-    public int getSingleDigit(int number, int repeatTimes) {
-        var numberAsString = Integer.toString(number).repeat(repeatTimes);
-        return getSingleDigit(numberAsString);
+    public int getSingleDigit(String number, int repeatTimes) {
+        if (number.matches("[0-9]*")) {
+            return getSingleDigit(repeatTimes <= 0 ? number : number.repeat(repeatTimes));
+        }
+        throw new RuntimeException();
     }
 
-    public int getSingleDigit(String number) {
-        if (number.matches("[0-9]*")) {
-            while (number.length() > 1) {
-                int sum = 0;
-                for (char digit : number.toCharArray()) {
-                    sum += Character.getNumericValue(digit);
-                }
-                number = Integer.toString(sum);
+    private int getSingleDigit(String number) {
+        while (number.length() > 1) {
+            int sum = 0;
+            for (char digit : number.toCharArray()) {
+                sum += Character.getNumericValue(digit);
             }
-            return Integer.parseInt(number);
-        } else {
-            throw new RuntimeException();
+            number = Integer.toString(sum);
         }
+        return Integer.parseInt(number);
     }
 }
--- src/test/java/com/example/digitounico/service/SingleDigitServiceTest.java
+++ src/test/java/com/example/digitounico/service/SingleDigitServiceTest.java
@@ -14,16 +14,26 @@ public class SingleDigitServiceTest {
 
     @Test
     public void shouldThrowException_whenStringHasANonDigitChar() {
-        assertThrows(RuntimeException.class, () -> singleDigitService.getSingleDigit("sasa7s4as"));
+        assertThrows(RuntimeException.class, () -> singleDigitService.getSingleDigit("sasa7s4as", 1));
     }
 
     @Test
-    public void shouldReturn2_whenStringInputIs9875() {
-        assertEquals(2, singleDigitService.getSingleDigit("9875"));
+    public void shouldReturn2_whenInputsAreNumber9875AndRepeatTimes1() {
+        assertEquals(2, singleDigitService.getSingleDigit("9875", 1));
     }
 
     @Test
-    public void shouldReturn8_whenInputsAre9875And4() {
-        assertEquals(8, singleDigitService.getSingleDigit(9875, 4));
+    public void shouldReturn8_whenInputsAreNumber9875AndRepeatTimes4() {
+        assertEquals(8, singleDigitService.getSingleDigit("9875", 4));
+    }
+
+    @Test
+    public void shouldReturnSingleDigitWithoutRepeatingInputNumber_whenInputRepeatTimeIsZero() {
+        assertEquals(2, singleDigitService.getSingleDigit("9875", 0));
+    }
+
+    @Test
+    public void shouldReturnSingleDigitWithoutRepeatingInputNumber_whenInputRepeatTimeIsLessThanZero() {
+        assertEquals(2, singleDigitService.getSingleDigit("9875", -1));
     }
 }
