a83461fcb1ad1449bdcc71fca816e2718b6a4616
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-11 15:25:56 -0300

    mocking cache service on tests

--- src/test/java/com/example/digitounico/service/SingleDigitServiceTest.java
+++ src/test/java/com/example/digitounico/service/SingleDigitServiceTest.java
@@ -2,15 +2,24 @@ package com.example.digitounico.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SingleDigitServiceTest {
 
-    SingleDigitService singleDigitService = new SingleDigitService();
+    @Mock
+    private SingleDigitCacheService cache;
+
+    @InjectMocks
+    private SingleDigitService singleDigitService;
 
     @Test
     public void shouldThrowException_whenStringHasANonDigitChar() {
@@ -19,21 +28,30 @@ public class SingleDigitServiceTest {
 
     @Test
     public void shouldReturn2_whenInputsAreNumber9875AndRepeatTimes1() {
+        setupCacheToReturnNull();
         assertEquals(2, singleDigitService.getSingleDigit("9875", 1));
     }
 
     @Test
     public void shouldReturn8_whenInputsAreNumber9875AndRepeatTimes4() {
+        setupCacheToReturnNull();
         assertEquals(8, singleDigitService.getSingleDigit("9875", 4));
     }
 
     @Test
     public void shouldReturnSingleDigitWithoutRepeatingInputNumber_whenInputRepeatTimeIsZero() {
+        setupCacheToReturnNull();
         assertEquals(2, singleDigitService.getSingleDigit("9875", 0));
     }
 
     @Test
     public void shouldReturnSingleDigitWithoutRepeatingInputNumber_whenInputRepeatTimeIsLessThanZero() {
+        setupCacheToReturnNull();
         assertEquals(2, singleDigitService.getSingleDigit("9875", -1));
+    }
+
+    private void setupCacheToReturnNull() {
+        when(cache.get(any())).thenReturn(null);
+        doNothing().when(cache).store(anyString(), anyInt());
     }
 }
