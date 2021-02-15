43dd906fbc5a1e353d9723e15df7d80276488188
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-15 20:59:09 -0300

    single digits controller

--- src/main/java/com/example/digitounico/controllers/SingleDigitController.java
+++ src/main/java/com/example/digitounico/controllers/SingleDigitController.java
@@ -1,7 +1,23 @@
 package com.example.digitounico.controllers;
 
+import com.example.digitounico.entities.dto.SingleDigitResponse;
+import com.example.digitounico.services.SingleDigitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class SingleDigitController {
+
+    private final SingleDigitService singleDigitService;
+
+    @GetMapping("/single-digits")
+    public ResponseEntity create(@RequestParam("integer") String integer,
+                                 @RequestParam("repeat-times") Integer repeatTimes) {
+        var singleDigit = singleDigitService.getSingleDigit(integer, repeatTimes);
+        return ResponseEntity.ok(new SingleDigitResponse(singleDigit));
+    }
 }
---
+++ src/main/java/com/example/digitounico/entities/dto/SingleDigitResponse.java
@@ -0,0 +1,12 @@
+package com.example.digitounico.entities.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class SingleDigitResponse {
+    private final Integer singleDigit;
+}
