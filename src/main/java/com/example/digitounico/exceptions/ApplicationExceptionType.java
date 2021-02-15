27d0205f11ef7ee9677ff9e186af79b707658945
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-15 17:27:55 -0300

    fixing rsa key error message

--- src/main/java/com/example/digitounico/exceptions/ApplicationExceptionType.java
+++ src/main/java/com/example/digitounico/exceptions/ApplicationExceptionType.java
@@ -10,7 +10,7 @@ public enum ApplicationExceptionType {
 
     ENTITY_NOT_FOUND("Entidade não encontrada.", HttpStatus.NOT_FOUND),
     EMAIL_ALREADY_USED("Email já cadastrado.", HttpStatus.CONFLICT),
-    INVALID_RSA_KEY("Chave RSA mal formada, verifique se este dato está codificado em base 64.", HttpStatus.BAD_REQUEST),
+    INVALID_RSA_KEY("Chave RSA mal formada, verifique se esse dado está codificado em base 64.", HttpStatus.BAD_REQUEST),
     TOO_LONG_DATA_TO_BE_ENCRYPTED("Informação muito longa para ser criptografada pela chave.", HttpStatus.BAD_REQUEST),
     TOO_LONG_DATA_TO_BE_DECRYPTED("Informação muito longa para ser descriptografada pela chave.", HttpStatus.BAD_REQUEST),
     INTERNAL_ERROR("Erro interno da aplicação.", HttpStatus.INTERNAL_SERVER_ERROR);
