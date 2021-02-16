c14809fff9d302ff4b3c9e50157ae8610772176b
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-15 21:05:33 -0300

    changing sql files location

--- src/main/java/com/example/digitounico/repositories/queries/AppUserQuery.java
+++ src/main/java/com/example/digitounico/repositories/queries/AppUserQuery.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 
 @Getter
 public enum AppUserQuery {
-    INSERT("appuser/insert.sql"),
-    UPDATE("appuser/update-by-uid.sql"),
-    SELECT_ALL("appuser/select-all.sql"),
-    SELECT_BY_UID("appuser/select-by-uid.sql"),
-    SELECT_BY_EMAIL("appuser/select-by-email.sql"),
-    DELETE("appuser/delete-by-uid.sql");
+    INSERT("insert.sql"),
+    UPDATE("update-by-uid.sql"),
+    SELECT_ALL("select-all.sql"),
+    SELECT_BY_UID("select-by-uid.sql"),
+    SELECT_BY_EMAIL("select-by-email.sql"),
+    DELETE("delete-by-uid.sql");
 
     private final String query;
 
