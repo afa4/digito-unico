77461f670f3cd5e0c1279015f19ffdd96cf40b71
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-18 12:04:28 -0300

    removing wrong validation

--- src/main/java/com/example/digitounico/services/UsersCryptoService.java
+++ src/main/java/com/example/digitounico/services/UsersCryptoService.java
@@ -1,15 +1,11 @@
 package com.example.digitounico.services;
 
 import com.example.digitounico.entities.AppUser;
-import com.example.digitounico.exceptions.ApplicationException;
 import com.example.digitounico.utils.CryptoUtil;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
-
-import static com.example.digitounico.exceptions.ApplicationExceptionType.USER_DATA_ALREADY_ENCRYPTED;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +16,6 @@ public class UsersCryptoService {
     public AppUser encrypt(UUID userUid, String base64EncodedPublicKey) {
 
         var user = usersCrudService.findByUid(userUid);
-
-        if (Base64.isBase64(user.getName()))
-            throw new ApplicationException(USER_DATA_ALREADY_ENCRYPTED);
 
         user.setName(CryptoUtil.encrypt(user.getName(), base64EncodedPublicKey));
         user.setEmail(CryptoUtil.encrypt(user.getEmail(), base64EncodedPublicKey));
